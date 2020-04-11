50e6df9e5b77c6382469a03b4511cf62d68f119c
Alexis Metzler <metzlera@umich.edu>
2020-04-11 16:16:34 -0400

    did some stuff, we'll see

--- APIStuff.py
+++ APIStuff.py
@@ -1,1 +1,111 @@
+'''
+Game Plan:
+1) Make a list of Countries
+2) Get the date of their first case
+3) Get Date of 50th case
+4) find number of days between 1 and 50
+5) put in all in a csv on github
+    list of (country name , days from day 1 to day 50)
+6) maybe also create a json file of day by day number of cases
+    {country name:[(day,case),(day,case)...]}
 
+Functions to make:
+1) Count days 
+    Turn date into quantifiable thing
+    maybe just have an iterator that starts at day 1 stops when cases == 50
+2) Fetch data from API
+3) Write to database
+4) Functions to grab from past projects/homeworks
+    setUpDatabase       HW8
+    main                kinda everywhere
+    write_csv           Project 2
+
+'''
+
+# AND SO IT BEGINS
+# Get some imports, never sure what you might need
+import os
+import requests
+import json
+import re
+import csv
+
+# Task 1: Get a list of country names
+def get_country_names():
+    country_list = []
+    resp = requests.get('https://api.covid19api.com/countries')
+    data = json.loads(resp.text)
+    
+    for country in data:
+        country_list.append(country['Country'])
+    
+    return country_list
+
+
+# Task 2: create dictionary of country:tuple list of (day,case) pairs
+def get_days():
+    country_dic={}
+    lst = get_country_names()
+    for country in lst:
+        count=0
+        day_tups = []
+        url = f'https://api.covid19api.com/dayone/country/{country}/status/confirmed'
+        resp = requests.get(url)
+        data = json.loads(resp.text)
+        if data == {"message":"Not Found"}:
+            continue
+        for day in data:
+            day_tups.append((count,day['Cases']))
+            count+=1
+        country_dic[country] = day_tups
+    return country_dic
+
+# Task 2.1 rewrite task 2 so that I can later call the function on a country and get the tuple list
+def country_days(country):
+    count=0
+    day_tups = []
+    url = f'https://api.covid19api.com/dayone/country/{country}/status/confirmed'
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+    if data == {"message":"Not Found"}:
+        print('No data found')
+        return None
+    for day in data:
+        day_tups.append((count,day['Cases']))
+        count+=1
+    return day_tups
+
+# Task 3: Get days from 1 reported case to 50
+def days_to_50(country):
+    count=0
+    tups = country_days(country)
+    if tups == None:
+        return None
+    for day in tups:
+        if day[1] >50:
+            return count
+        count+=1
+
+# Task 3: Write a csv of (country name, days to 50)
+def write_csv(filename):
+    full_path = os.path.join(os.path.dirname(__file__), filename)
+    fle = open(full_path,'w')
+    fle.write('country,days to 50')
+    data = get_country_names()
+    for country in data:
+        if days_to_50(country) == None:
+            continue
+        fle.write(f'\n{country},{days_to_50(country)}
+    fle.close()
+
+def main():
+    #print(get_country_names()[:5])
+    #print(get_days()['India'])
+    print(country_days("India"))
+    print(days_to_50('India'))
+    print('done')
+
+
+
+if __name__ == "__main__":
+    main()
