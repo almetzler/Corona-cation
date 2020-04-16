48f357b6a735e0d0f271da1a3e7c5521adc706a8
Alexis Metzler <metzlera@umich.edu>
2020-04-16 13:37:58 -0400

    added try-except to allow for errors

--- Visualization_1.py
+++ Visualization_1.py
@@ -20,31 +20,42 @@ def get_tups(country):
         y_vals.append(line[1])
     return x_vals, y_vals
 
-def plot_progessions(country):
-    x,y=get_tups(country)
-    plt.plot(x,y)
-    plt.xlabel('days since case 1')
-    plt.ylabel('cases confirmed')
-    plt.title(f'CORONA-CATION ({country})')
-    plt.show()
-    
-def plot_progessions_list(country_list,topvalue=None):
-    for country in country_list:
+def plot_progessions(country,topvalue=None):
+    try:
         x,y=get_tups(country)
         plt.plot(x,y)
-    plt.legend(country_list)
-    plt.xlabel('days since case 1')
-    plt.ylabel('cases confirmed')
-    plt.title(f'CORONA-CATION ({country})')
-    if topvalue:
-        plt.ylim(-1*topvalue/500,topvalue)
-    plt.show()
+        plt.xlabel('days since case 1')
+        plt.ylabel('cases confirmed')
+        plt.title(f'CORONA-CATION ({country})')
+        if topvalue:
+            plt.ylim(-1*topvalue/500,topvalue)
+        plt.show()
+    except:
+        print("error, something went wrong")
+    
+def plot_progessions_list(country_list,topvalue=None):
+    try:
+        title=''
+        for country in country_list:
+            x,y=get_tups(country)
+            plt.plot(x,y)
+            title=title+country+', '
+        plt.legend(country_list)
+        plt.xlabel('days since case 1')
+        plt.ylabel('cases confirmed')
+        plt.title(f'CORONA-CATION ({title[:-2]})')
+        if topvalue:
+            plt.ylim(-1*topvalue/500,topvalue)
+        plt.show()
+    except:
+        print('error, something went wrong')
     
 
 def main():
-    #plot_progessions("India")
-    #plot_progessions("Germany")
-    plot_progessions_list(["India","Germany",'United States of America'],5000)
+    #plot_progessions("India") produces wanted figure
+    #plot_progessions("Gerany") gives error
+    #plot_progessions_list(["India","Germany",'United States of America'],5000) produces correct figure
+    #plot_progessions_list(["India","Germay",'United States of America'],5000) gives error
     
 
 
